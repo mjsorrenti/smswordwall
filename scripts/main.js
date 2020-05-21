fc12a3a4df5cbf291561cd9f8c8b61ac3760ab85
mjsorrenti <31044756+mjsorrenti@users.noreply.github.com>
2020-05-21 14:18:05 -0400

    Replace missing comma

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var words = {text:["a","all","am","an","and","are","as","at","be","big","by","ca
                   "did","do", "each", "find","for","from","get","go","had","have","he","help","here",
                   "his","how","I",
                   "in","is","it","like","look","love","make","me","my","new","no","not","of","on",
-                  "or","out","play","said","see","she","that","the","there","this","to","up","use"
+                  "or","out","play","said","see","she","that","the","there","this","to","up","use",
                    "was","we","were","when","will","with","what","yes","you","your"],
              numbers:["(1) one","(2) two","(3) three","(4) four","(5) five","(6) six","(7) seven",
                       "(8) eight","(9) nine","(10) ten"],
